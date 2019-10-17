cdb9be4c180baa96cba8f4a9168f9e1d393b6858
edd26 <ed18abc@herts.ac.uk>
2019-10-17 11:19:34 +0100

    Fixed path changing problem; removed default result path

--- test_on_numbered_matrix.jl
+++ test_on_numbered_matrix.jl
@@ -12,9 +12,10 @@ using Plots
  Plot Betti curves from 0 up to 3 using results from Eirene library and returns
  handler for figure. Optionally, save the figure
  """
-function plot_and_save_bettis(eirene_results, plot_title, data_size;
-                                do_save=false, do_normalise=true,
-             save_path = "/home/ed19aaf/Programming/Julia/eirene-tests/results")
+function plot_and_save_bettis(eirene_results, plot_title,
+                                data_size, results_path;
+                                do_save=false, do_normalise=true)
+
      betti_0 = betticurve(eirene_results, dim=0)
      betti_1 = betticurve(eirene_results, dim=1)
      betti_2 = betticurve(eirene_results, dim=2)
@@ -30,7 +31,8 @@ function plot_and_save_bettis(eirene_results, plot_title, data_size;
      cur_colors = get_color_palette(:auto, plot_color(:white), 17)
 
      final_title = "Eirene betti curves, "*plot_title*" data, size "*data_size
-     p1 = plot(betti_0[:,1], betti_0[:,2], label="\\beta_0", lc=cur_colors[7], title=final_title);
+     p1 = plot(betti_0[:,1], betti_0[:,2], label="\\beta_0", lc=cur_colors[7],
+                                                        title=final_title);
      plot!(betti_1[:,1], betti_1[:,2], label="\\beta_1", lc=cur_colors[5]);
      plot!(betti_2[:,1], betti_2[:,2], label="\\beta_2", lc=[:red]);
      plot!(betti_3[:,1], betti_3[:,2], label="\\beta_3", lc=cur_colors[1]);
@@ -40,8 +42,10 @@ function plot_and_save_bettis(eirene_results, plot_title, data_size;
      ylabel!("Number of cycles")
 
      if do_save
-         cd(save_path)
+         current_path = pwd()
+         cd(results_path)
          savefig(plot_ref, "betti_curves_"*plot_title*data_size*".png")
+         cd(current_path)
      end
 
  end
@@ -52,6 +56,8 @@ eirene_maxdim = 3;
 
 n_numbered_matrix="numbered_matrix";
  data_size = "10"
+ result_path = "./results/"
+ figure_path = "figures/"
 
 data_path = "./data/"
  suffix = "_size"
@@ -64,4 +70,5 @@ numbered_matrix = readdlm(data_path*n_numbered_matrix*suffix*data_size*file_form
 res_eirene_numbered_matrix = eirene(numbered_matrix,maxdim=eirene_maxdim,
                                                             model=eirene_model)
 
-plot_and_save_bettis(res_eirene_numbered_matrix, n_numbered_matrix, data_size, do_save=true)
+plot_and_save_bettis(res_eirene_numbered_matrix, n_numbered_matrix, data_size,
+                                result_path*figire_path, do_save=true)
