f8d730a75066093726fdd12adfe9918dd64c7032
edd26 <ed18abc@herts.ac.uk>
2020-01-24 16:21:29 +0000

    Adjusted testing parameters

--- multiscale_matrix_testing_dimension.jl
+++ multiscale_matrix_testing_dimension.jl
@@ -27,7 +27,7 @@ cd("../eirene-tests")
 # ==============================================
 # ============= matrix parameters ==============
 dims = collect(5:5:105)
-    repetitions = 20
+    repetitions = 100
     min_B_dim = 1
     max_B_dim = 3
     size_start = 10
