c01601b0699a5ea5bf8563b7f0a3f7d02fcfee9d
edd26 <ed18abc@herts.ac.uk>
2020-01-24 16:29:55 +0000

    Added performance testing for scripts

--- all_testing.jl
+++ all_testing.jl
@@ -1,2 +1,9 @@
-include("multiscale_matrix_testing.jl")
-include("multiscale_matrix_testing_dimension.jl")
+val, t, bytes, gctime, memallocs = @timed include("multiscale_matrix_testing.jl")
+save("all_test_pt1.jld", "val",val, "t",t, "bytes",bytes, "gctime",gctime,
+                                                        "memallocs",memallocs)
+
+
+
+val, t, bytes, gctime, memallocs = @timed include("multiscale_matrix_testing_dimension.jl")
+save("all_test_pt2.jld", "val",val, "t",t, "bytes",bytes, "gctime",gctime,
+                                                        "memallocs",memallocs)
