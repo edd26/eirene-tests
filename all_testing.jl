bf2743c330056a3050a0d48ee7281660800230d2
edd26 <ed18abc@herts.ac.uk>
2020-01-27 10:11:29 +0000

    swapped tests order

--- all_testing.jl
+++ all_testing.jl
@@ -1,10 +1,8 @@
-
-val, t, bytes, gctime, memallocs = @timed include("multiscale_matrix_testing_dimension.jl")
-save("all_test_pt2.jld", "val",val, "t",t, "bytes",bytes, "gctime",gctime,
+val, t, bytes, gctime, memallocs = @timed include("multiscale_matrix_testing.jl")
+save("all_test_pt1.jld", "val",val, "t",t, "bytes",bytes, "gctime",gctime,
                                                         "memallocs",memallocs)
 
 
-
-val, t, bytes, gctime, memallocs = @timed include("multiscale_matrix_testing.jl")
-save("all_test_pt1.jld", "val",val, "t",t, "bytes",bytes, "gctime",gctime,
+val, t, bytes, gctime, memallocs = @timed include("multiscale_matrix_testing_dimension.jl")
+save("all_test_pt2.jld", "val",val, "t",t, "bytes",bytes, "gctime",gctime,
                                                         "memallocs",memallocs)
